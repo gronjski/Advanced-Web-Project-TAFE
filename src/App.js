4e7745b610373b9f17a88d84411dda4192718685
Nikola Mitic <gronjski@gmail.com>
2023-07-09 18:24:11 +1000

    Added a button from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {initializeApp} from 'firebase/app'
 import {firebaseConfig} from './config/Config'
 import './App.css';
+import { Test } from './Components/Test';
+
+//const FirebaseApp = initializeApp(firebaseConfig)
 
 function App() {
   return (
     <div className="App">
-
+        <Test />
     </div>
   );
 }
