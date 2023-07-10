b5ee061917019ad31669c799fa050a2006aa787c
Nikola Mitic <gronjski@gmail.com>
2023-07-10 15:39:42 +1000

    Installed React-Router-Dom and added pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import {initializeApp} from 'firebase/app'
 import {firebaseConfig} from './config/Config'
 import './App.css';
 import { Test } from './Components/Test';
+import { Home } from './Pages/Home';
+import { About } from './Pages/About';
+import {Routes, Route} from 'react-router-dom'
 
-//const FirebaseApp = initializeApp(firebaseConfig)
+const FirebaseApp = initializeApp(firebaseConfig)
 
 function App() {
   return (
