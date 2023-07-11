17606555a500339dd29c4f657252b51cb806b845
Nikola Mitic <gronjski@gmail.com>
2023-07-11 10:32:20 +0200

    Added new component to route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import {initializeApp} from 'firebase/app'
 import {firebaseConfig} from './config/Config'
 import './App.css';
 import { Test } from './Components/Test';
-import { Home } from './Pages/Home';
-import { About } from './Pages/About';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
 import {Routes, Route} from 'react-router-dom'
 
 const FirebaseApp = initializeApp(firebaseConfig)
@@ -13,7 +14,8 @@ function App() {
     <div className="App">
         <Routes>
           <Route path="/" element={ <Home/> } />
-          <Route path="/" element={ <About/> } />
+          <Route path="/about" element={ <About/> } />
+          <Route path="/contact" element={ <Contact/> } />
         </Routes>
     </div>
   );
