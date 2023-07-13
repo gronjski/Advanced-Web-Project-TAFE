c791a44cc0215b5987582c0474c071039295cd4a
Nikola Mitic <gronjski@gmail.com>
2023-07-13 08:29:55 +0200

    Added Navbar components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
-import Navbar from "react-bootstrap";
-import Container from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 export function Header ( props ) {
     return (
-        <Navbar>
+        <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand>
                     Hello World
                 </Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-nav" />
+                <Navbar.Collapse id="main-nav"></Navbar.Collapse>
             </Container>
         </Navbar>
     )
