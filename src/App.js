bc010e81c616b47f5578eea99db2d726a85d560c
Nikola Mitic <gronjski@gmail.com>
2023-08-17 00:40:42 +1000

    adding storage context in app and home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import {initializeApp} from 'firebase/app'
 import {firebaseConfig} from './config/Config'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import firestore
 import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage";
 import { useState } from 'react';
 
 import './App.css';
@@ -19,10 +21,12 @@ import {Routes, Route} from 'react-router-dom';
 import { NavContext } from './contexts/NavContext';
 import { FBAuthContext } from './contexts/FBAuthContext';
 import { FBDbContext } from './contexts/FBDbContext';
+import { FBStorageContext } from './contexts/FBStorageContext';
 
 const FirebaseApp = initializeApp(firebaseConfig)
 const FirebaseAuth = getAuth (FirebaseApp)
 const FirebaseDB = getFirestore (FirebaseApp)
+const FirebaseStorage = getStorage(FirebaseApp)
 
 const NavRoutes = [
   {name: "Home", goto: "/"},
@@ -61,7 +65,8 @@ function App() {
         <Header/>
       </NavContext.Provider>
       <FBAuthContext.Provider value={FirebaseAuth}>
-        <FBDbContext.Provider value={FirebaseDB}>
+      <FBDbContext.Provider value={FirebaseDB}>
+      <FBStorageContext.Provider value ={FirebaseStorage}>
         <Routes>
             <Route path="/" element={ <Home/> } />
             <Route path="/home" element={ <Home/> } />
@@ -71,7 +76,8 @@ function App() {
             <Route path="/signin" element={ <Signin/> } />
             <Route path="/signout" element={ <Signout/>} />
         </Routes>
-        </FBDbContext.Provider>
+      </FBStorageContext.Provider>
+      </FBDbContext.Provider>
       </FBAuthContext.Provider>
     </div>
   );
